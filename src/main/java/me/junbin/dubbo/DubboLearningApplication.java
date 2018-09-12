1e4e7a0ba98c0a824d3dfa3226d3b89ac762def6
钟俊滨 <rekadowney@163.com>
2018-09-12 22:29:02 +0800

    初始化项目

---
+++ src/main/java/me/junbin/dubbo/DubboLearningApplication.java
@@ -0,0 +1,13 @@
+package me.junbin.dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboLearningApplication.class, args);
+    }
+
+}
